f737acf81f166b6f3e2004d6eff5a4940bf5b181
JRissberger <112580719+JRissberger@users.noreply.github.com>
2025-01-28 17:35:43 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,163 @@
+window.onload = ()=>{
+   document.querySelector("#previous").onclick = viewPrevious;
+   document.querySelector("#next").onclick = viewNext;
+   document.querySelector("#galleryPrevious").onclick = viewGalleryPrevious;
+   document.querySelector("#galleryNext").onclick = viewGalleryNext;
+   initialProjectView();
+}
+
+//counter for current featured image and total images
+let galleryView = 0;
+let galleryTotal = 5;
+
+//counter for current project view and total number of projects
+let projectView = 0;
+let totalProjects = 3;
+
+//scrolls between gallery images
+function viewGalleryNext()
+{
+    galleryView++;
+
+    //rolls over to min value if needed
+    if (galleryView > galleryTotal - 1)
+    {
+        galleryView = 0;
+    }
+
+    //loads gallery image
+    viewGalleryImage();
+}
+function viewGalleryPrevious()
+{
+    galleryView--;
+
+    //rolls over to max value if needed
+    if (galleryView < 0)
+    {
+        galleryView = galleryTotal - 1;
+    }
+
+    //loads view
+    viewGalleryImage();
+}
+
+//displays image in featured gallery
+function viewGalleryImage()
+{
+    //switch to check counter value
+    switch(galleryView)
+    {
+        case 0:
+            document.querySelector("#galleryImg").innerHTML = "<img src = 'media/AOTitle.png' alt = 'Astro Outlaw title screen'></img>";
+            break;
+        case 1:
+            document.querySelector("#galleryImg").innerHTML = "<img src = 'media/AOScreenshot1.png' alt = 'Astro Outlaw gameplay'></img>";
+            break;
+        case 2:
+            document.querySelector("#galleryImg").innerHTML = "<img src = 'media/AOScreenshot2.png' alt = 'Astro Outlaw gameplay'></img>";
+            break;
+        case 3:
+            document.querySelector("#galleryImg").innerHTML = "<img src = 'media/AOScreenshot3.png' alt = 'Astro Outlaw gameplay'></img>";
+            break;
+        case 4:
+            document.querySelector("#galleryImg").innerHTML = "<img src = 'media/AOScreenshot4.png' alt = 'Astro Outlaw gameplay'></img>";
+            break;
+        default:
+            document.querySelector("#galleryImg").innerHTML = "image not found";
+    }
+}
+//scrolls to previous project view
+function viewPrevious(){
+    //decreases counter by 1
+    projectView--;
+
+    //Checks value, rolls over to max value if needed
+    if (projectView < 0)
+    {
+        projectView = totalProjects - 1;
+    }
+
+    //loads project view
+    viewProject();
+}
+
+//scrolls to next project view
+function viewNext(){
+
+    //increments counter by 1
+    projectView++;
+
+    //checks view value, adjusts value if needed to prevent overflow
+    if (projectView >= totalProjects)
+    {
+        projectView = 0;
+    }
+
+    //loads project view
+    viewProject();
+}
+
+//displays new project
+function viewProject()
+{
+    //switch to check counter value
+    switch(projectView)
+    {
+        case 0:
+            document.querySelector("#othercontent").innerHTML = 
+            "TEST TEST TEST";
+            break;
+        //card flip game
+        case 1:
+            document.querySelector("#othercontent").innerHTML = 
+            "<h5>Card Flip Game</h5>" +
+            "<p>In this unofficial Pokemon fan game, match as many cards as you can before you run out of moves or time." +
+                " Scoring is based on the elemental type the player is assigned, and how it matches up against the flipped card's elemental type. </p>" +
+            "<ul>" +
+                "<li>Solo project created using HTML, CSS, and JavaScript.</li>" +
+                "<li>Utilized manipulation of CSS class lists to simulate cards being flipped, and managed the board and card states with JavaScript.</li>" +
+                "<li>JavaScript was also used to track the current state of the game, shuffle the cards, and reset the board/end the game as needed. </li>" +
+                "<li><a href = 'https://people.rit.edu/jbr4368/235/project3/'>Site Link</a></li>" + 
+            "</ul>";
+            document.querySelector("#otherimg").innerHTML = 
+            "<img src = 'media/cardgame.png' alt = 'gameplay screenshot of card game'></img>";
+            break;
+
+        //possum parenthood
+        case 2:
+            document.querySelector("#othercontent").innerHTML = "<h5>'Possum Parenthood</h5>" +
+            "<p>Play as a mother possum, sneaking around to find food for her children. Avoid the humans and hide in boxes to collect all the food" +
+            "before time runs out! But watch out--if you get caught, you'll lose precious time playing dead!" +
+        "</p><ul>" +
+            "<li>A group project made with 4 other students</li>" +
+            "<li>Programmed in C#, using Monogame</li>" +
+            "<li>Areas worked on: " +
+                "<ul>"+
+                    "<li>Initial class framework</li>"+
+                    "<li>Enemy movement</li>" +
+                    "<li>Enemy interaction with player</li> " +
+                    "<li>Player 'play dead'  mechanism</li>" +
+                    "<li>General bug fixes</li>"+
+                "</ul>"+
+            "</li><li><a href = 'https://github.com/macarenzie/ggg'>Github</a></li></ul>";
+            document.querySelector("#otherimg").innerHTML = "<img src = 'media/possum_gameplay.png' alt = 'gameplay screenshot of possum game'>";
+            break;
+        default:
+            document.querySelector("#othercontent").innerHTML = "project not found";
+            document.querySelector("#otherimg").innerHTML = "image not found";
+    }
+}
+
+//used to load the initial view
+function initialProjectView()
+{
+    //Featured project gallery TODO: add images
+    document.querySelector("#galleryImg").innerHTML = "<img src = 'media/AOTitle.png' alt = 'Astro Outlaw title screen'></img>";
+
+    //Other projects
+    document.querySelector("#othercontent").innerHTML = 
+    "TEST TEST TEST";
+    document.querySelector("#otherimg").innerHTML = 
+    "<img src = 'media/cardgame.png' alt = 'gameplay screenshot of card game'></img>";
+}
